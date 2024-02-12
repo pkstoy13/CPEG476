8d77ad4d0e7bea686b0a1d4d8d77e0db08c40356
Philip Stoyanov <pkstoyanov1@gmail.com>
2024-02-11 19:56:45 -0500

    intro c & c++

---
+++ string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string text = "Andy is the greatest";
+  cout << text << endl;
+  cout << text.length() << endl;
+}
